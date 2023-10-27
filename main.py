cb58dc55f3bc9da930df93fdf3b1db001fb16923
Zorro Byte <zorrobyte@gmail.com>
2023-10-27 02:45:12 -0400

    Move temp sensors to own module

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import machine
-import math
 import time
 import _thread
 import config
 import startup
 import shutdown
+import tempSensors
 
 
 # Initialize the WDT with a 10-second timeout
@@ -47,55 +47,6 @@ def pulse_fuel_thread():
 
 _thread.start_new_thread(pulse_fuel_thread, ())
 
-# Constants for 50k NTC thermistor
-BETA_NTC_50k = 3950  # Placeholder value; you should calibrate this for more accuracy
-R0_NTC_50k = 50000.0  # 50k ohms
-T0_NTC_50k = 298.15  # 25C in Kelvin
-
-
-def read_output_temp():
-    try:
-        analog_value = config.OUTPUT_TEMP_ADC.read()
-        resistance = 1 / (4095.0 / analog_value - 1)
-
-        # Calculate temperature using the simplified B parameter equation for NTC
-        temperature_k = 1 / (math.log(resistance / R0_NTC_50k) / BETA_NTC_50k + 1 / T0_NTC_50k)
-
-        # Convert temperature to Celsius
-        celsius = temperature_k - 273.15
-        if config.IS_SIMULATION:
-            return 60
-        else:
-            return celsius
-    except Exception as e:
-        print("An error occurred while reading the output temperature sensor:", str(e))
-        return 999
-
-
-# Constants for 1K PTC thermistor (HCalory Coolant Heater)
-BETA_PTC_1K = 3000  # Placeholder value; you should calibrate this for more accuracy
-R0_PTC_1K = 1000.0  # 1k ohms
-T0_PTC_1K = 298.15  # 25C in Kelvin
-
-
-def read_exhaust_temp():
-    try:
-        analog_value = config.EXHAUST_TEMP_ADC.read()
-        resistance = 1 / (4095.0 / analog_value - 1)
-
-        # Calculate temperature using the simplified B parameter equation for PTC
-        temperature_k = 1 / (1 / T0_PTC_1K + (1 / BETA_PTC_1K) * math.log(resistance / R0_PTC_1K))
-
-        # Convert temperature to Celsius
-        celsius = temperature_k - 273.15
-        if config.IS_SIMULATION:
-            return 60
-        else:
-            return celsius
-    except Exception as e:
-        print("An error occurred while reading the exhaust temperature sensor:", str(e))
-        return 999
-
 
 def control_air_and_fuel(output_temp, exhaust_temp):
     #  TODO IMPLEMENT FLAME OUT BASED ON exhaust_temp
@@ -105,7 +56,7 @@ def control_air_and_fuel(output_temp, exhaust_temp):
     fan_speed_percentage = min(max((delta / max_delta) * 100, config.MIN_FAN_PERCENTAGE), config.MAX_FAN_PERCENTAGE)
     fan_duty = int((fan_speed_percentage / 100) * 1023)
     config.pump_frequency = min(max((delta / max_delta) * config.MAX_PUMP_FREQUENCY, config.MIN_PUMP_FREQUENCY),
-                         config.MAX_PUMP_FREQUENCY)
+                                config.MAX_PUMP_FREQUENCY)
 
     config.air_pwm.duty(fan_duty)
     if config.IS_WATER_HEATER:
@@ -153,8 +104,8 @@ if config.USE_MQTT:
 
 
     def publish_sensor_values():
-        output_temp = read_output_temp()
-        exhaust_temp = read_exhaust_temp()
+        output_temp = tempSensors.read_output_temp()
+        exhaust_temp = tempSensors.read_exhaust_temp()
         payload = {
             "output_temp": output_temp,
             "exhaust_temp": exhaust_temp
@@ -205,8 +156,8 @@ def main():
                 except Exception as e:
                     print(f"Error reconnecting to MQTT: {e}")
 
-        output_temp = read_output_temp()
-        exhaust_temp = read_exhaust_temp()
+        output_temp = tempSensors.read_output_temp()
+        exhaust_temp = tempSensors.read_exhaust_temp()
         current_switch_value = config.SWITCH_PIN.value()
 
         # State transitions
---
+++ tempSensors.py
@@ -0,0 +1,51 @@
+import config
+import math
+
+# Constants for 50k NTC thermistor
+BETA_NTC_50k = 3950  # Placeholder value; you should calibrate this for more accuracy
+R0_NTC_50k = 50000.0  # 50k ohms
+T0_NTC_50k = 298.15  # 25C in Kelvin
+
+
+def read_output_temp():
+    try:
+        analog_value = config.OUTPUT_TEMP_ADC.read()
+        resistance = 1 / (4095.0 / analog_value - 1)
+
+        # Calculate temperature using the simplified B parameter equation for NTC
+        temperature_k = 1 / (math.log(resistance / R0_NTC_50k) / BETA_NTC_50k + 1 / T0_NTC_50k)
+
+        # Convert temperature to Celsius
+        celsius = temperature_k - 273.15
+        if config.IS_SIMULATION:
+            return 60
+        else:
+            return celsius
+    except Exception as e:
+        print("An error occurred while reading the output temperature sensor:", str(e))
+        return 999
+
+
+# Constants for 1K PTC thermistor (HCalory Coolant Heater)
+BETA_PTC_1K = 3000  # Placeholder value; you should calibrate this for more accuracy
+R0_PTC_1K = 1000.0  # 1k ohms
+T0_PTC_1K = 298.15  # 25C in Kelvin
+
+
+def read_exhaust_temp():
+    try:
+        analog_value = config.EXHAUST_TEMP_ADC.read()
+        resistance = 1 / (4095.0 / analog_value - 1)
+
+        # Calculate temperature using the simplified B parameter equation for PTC
+        temperature_k = 1 / (1 / T0_PTC_1K + (1 / BETA_PTC_1K) * math.log(resistance / R0_PTC_1K))
+
+        # Convert temperature to Celsius
+        celsius = temperature_k - 273.15
+        if config.IS_SIMULATION:
+            return 60
+        else:
+            return celsius
+    except Exception as e:
+        print("An error occurred while reading the exhaust temperature sensor:", str(e))
+        return 999
