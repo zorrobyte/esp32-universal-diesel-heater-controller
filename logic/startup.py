5d5778546e69052ae69a31ecdfeed830fd58d7d1
Zorro Byte <zorrobyte@gmail.com>
2023-10-28 00:23:07 -0400

    Dynamic startup procedure
    Should be dynamic

--- logic/startup.py
+++ logic/startup.py
@@ -1,6 +1,7 @@
 import config
 import time
-from logic import tempSensors, shutdown
+from logic import tempSensors, emergencyStop
+
 
 def start_up():
     step = 0
@@ -10,64 +11,78 @@ def start_up():
     glow_plug_heat_time = 0
     exhaust_check_time = 0
     exhaust_temps = []
+    avg_exhaust_temp = None  # Initialize to None
+
+    # Startup time limit (10 minutes)
+    startup_start_time = time.time()
+    startup_time_limit = 600  # 10 minutes in seconds
 
     while True:
-        config.heartbeat = time.time()  # Update heartbeat
+        config.heartbeat = time.time()
+
+        # Check if the startup process is taking too long
+        if time.time() - startup_start_time > startup_time_limit:
+            emergencyStop.emergency_stop("Startup took too long")
+            return
 
         if step == 0:
             print("Starting up...")
-            # Initial setup
             initial_exhaust_temp = tempSensors.read_exhaust_temp()
-            # Initialize fan and fuel pump
             fan_duty = int((fan_speed_percentage / 100) * 1023)
             config.air_pwm.duty(fan_duty)
             config.GLOW_PIN.on()
             glow_plug_heat_time = time.time()
+
             step += 1
 
         elif step == 1:
             print("Heating glow plug...")
-            # Check if 30 seconds have passed for the glow plug to heat
-            if time.time() - glow_plug_heat_time >= 30:
+            # If the initial temperature is below freezing, allow more time for the glow plug to heat
+            glow_plug_target_time = 30 if initial_exhaust_temp >= 0 else 60
+            if time.time() - glow_plug_heat_time >= glow_plug_target_time:
                 step += 1
 
         elif step == 2:
             print("Initializing fuel pump...")
-            # Start fuel pump with initial frequency
             config.pump_frequency = 1
             exhaust_check_time = time.time()
             step += 1
 
         elif step == 3:
             print("Checking exhaust temperature...")
-            # Record exhaust temperature for 20 seconds
             if time.time() - exhaust_check_time < 20:
                 exhaust_temps.append(tempSensors.read_exhaust_temp())
             else:
                 avg_exhaust_temp = sum(exhaust_temps) / len(exhaust_temps)
                 print(f"Average Exhaust Temp: {avg_exhaust_temp}°C")
 
-                if avg_exhaust_temp > initial_exhaust_temp:
-                    print("Exhaust temperature rising.")
-                    # Increase fan speed and fuel pump frequency
+                # Dynamic adjustment of min_temp_rise based on current average temperature
+                min_temp_rise = 5.0 if avg_exhaust_temp < 50 else 3.0
+
+                if avg_exhaust_temp > initial_exhaust_temp + min_temp_rise:
                     fan_speed_percentage = min(fan_speed_percentage + 20, 100)
                     fan_duty = int((fan_speed_percentage / 100) * 1023)
                     config.air_pwm.duty(fan_duty)
-
                     config.pump_frequency = min(config.pump_frequency + 1, 5)
-                    initial_exhaust_temp = avg_exhaust_temp  # Update for the next round
-                    exhaust_temps = []  # Clear recorded temperatures
-                    exhaust_check_time = time.time()  # Reset the timer
+                    initial_exhaust_temp = avg_exhaust_temp
+                    exhaust_temps = []
+                    exhaust_check_time = time.time()
                 else:
                     print("Temperature not rising as expected. Stopping fueling.")
                     config.current_state = 'STOPPING'
-                    config.startup_attempts += 1  # Increment the failed attempts counter
+                    config.startup_attempts += 1
                     return
 
         elif step == 4:
-            print("Startup Procedure Completed")
-            config.startup_successful = True  # Set the flag to true as startup was successful
-            config.startup_attempts = 0  # Reset the failed attempts counter
-            break
+            if avg_exhaust_temp and avg_exhaust_temp >= 150:
+                print("Startup Procedure Completed")
+                config.startup_successful = True
+                config.startup_attempts = 0
+                break
+            else:
+                print(f"Startup failed, exhaust temperature is too low: {avg_exhaust_temp}°C")
+                config.current_state = 'STOPPING'
+                config.startup_attempts += 1
+                return
 
-        time.sleep(1)  # Sleep for a short while before the next iteration
+        time.sleep(1)
