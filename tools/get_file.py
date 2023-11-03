600c857e9bd371995b57260382bdc4686c0e03a0
Zorro Byte <zorrobyte@gmail.com>
2023-11-03 00:25:17 -0400

    Create get_file.py

---
+++ tools/get_file.py
@@ -0,0 +1,21 @@
+from ampy.pyboard import Pyboard
+from ampy.files import Files
+
+# Replace 'COM5' with the appropriate port on your system
+port = 'COM5'
+
+def get_file(filename):
+    try:
+        pyb = Pyboard(port)
+        files = Files(pyb)
+        contents = files.get(filename)
+        with open(filename, 'wb') as file:
+            file.write(contents)
+        print(f"File {filename} has been successfully downloaded.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        pyb.close()
+
+# Call the function with the name of the file you want to retrieve
+get_file('config.py')
